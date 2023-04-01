fa6137b3355478e3f3ba86afb14c2a7a5accad16
Nickname <88354270+MrNickn4me@users.noreply.github.com>
2023-04-01 14:37:01 +0200

    Delay on duct bounce

--- Assets/Scripts/Tapes/BounceWorldTape.cs
+++ Assets/Scripts/Tapes/BounceWorldTape.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 public class BounceWorldTape : BaseWorldTape
 {
     public float BounceForce = 500;
+    public float timer = 0;
+    public void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
     public override void OnPlayerStay(Player player, ContactPoint2D contact)
     {
         // Do player bounce. Make sure bouncing function have cooldown
         // so if collider bug occurs then player won't fly out of the map.
-        player.UseRigidbody.AddForce(-contact.normal.normalized * BounceForce);
-        player.UseRigidbody.AddForce(Physics2D.gravity);
+        if (timer > .2)
+        {
+            player.UseRigidbody.AddForce(-contact.normal.normalized * BounceForce);
+            player.UseRigidbody.AddForce(Physics2D.gravity);
+            timer = 0;
+        }
     }
 }
