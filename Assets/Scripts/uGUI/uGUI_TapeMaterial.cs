831b44c7e585165914442c3815034b309bff866e
Polybroo <viktor.chernikov@onet.pl>
2023-04-02 19:46:29 +0200

    uGUI_TapeMaterial now works

--- Assets/Scripts/uGUI/uGUI_TapeMaterial.cs
+++ Assets/Scripts/uGUI/uGUI_TapeMaterial.cs
@@ -8,22 +8,14 @@ public class uGUI_TapeMaterial : MonoBehaviour
     public Text Title;
     public Text Length;
 
-
-    private void Start()
+    private void FixedUpdate()
     {
-        DuctTapeRef = Player.Main.DuctTape;
-        DuctTapeRef.OnMaterialChange += UpdateIcon;
-        DuctTapeRef.OnLengthChange += UpdateLength;
-        UpdateIcon(DuctTapeRef.CurrentMaterial);
-    }
-    private void UpdateIcon(DuctTapeMaterial Material)
-    {
-        Icon.color = DuctTapeRef.ModeRef.GetColor();
-        Title.text = string.Format("{0} Tape", DuctTapeRef.ModeRef.Name);
-        UpdateLength(0, DuctTapeRef.ModeRef.Length);
-    }
-    private void UpdateLength(int oldLength, int newLength)
-    {
-        Length.text = string.Format("{0} meters left", DuctTapeRef.ModeRef.Length);
+        if (Player.Main != null)
+        {
+            DuctTape d = Player.Main.DuctTape;
+            Icon.color = d.ModeRef.GetColor();
+            Title.text = string.Format("{0} Tape", d.ModeRef.Name);
+            Length.text = string.Format("{0} meters left", d.ModeRef.Length);
+        }
     }
 }
